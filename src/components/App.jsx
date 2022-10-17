10b17f8726caa2a2081926681908604bc38f31e8
ihumentsev <a.ihumentsev@gmail.com>
2022-10-17 10:37:45 +0200

    lasy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import css from '../components/App.module.css';
-import Register from 'pages/Register/Register';
-import Login from 'pages/Login/Login';
-import Contacts from 'pages/Contacts/Contacts';
-import Home from 'pages/Home/Home';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { featchCurrentUser } from 'redux/authOperatione';
 import PrivatRoute from './PrivatRoute';
 import PublicRoute from './PublicRoute';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Contacts = lazy(() => import('pages/Contacts/Contacts'));
+const Register = lazy(() => import('pages/Register/Register'));
+const Login = lazy(() => import('pages/Login/Login'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -19,22 +21,24 @@ export const App = () => {
   }, [dispatch]);
   return (
     <div className={css.container}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route element={<PublicRoute restricted />}>
-            <Route index element={<Home />} />
+      <Suspense fallback={<h1>Загрузка...</h1>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route element={<PublicRoute restricted />}>
+              <Route index element={<Home />} />
+            </Route>
+            <Route element={<PublicRoute restricted />}>
+              <Route path="/register" element={<Register />} />
+            </Route>
+            <Route element={<PublicRoute restricted />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route element={<PrivatRoute />}>
+              <Route path="/contacts" element={<Contacts />}></Route>
+            </Route>
           </Route>
-          <Route element={<PublicRoute restricted />}>
-            <Route path="/register" element={<Register />} />
-          </Route>
-          <Route element={<PublicRoute restricted />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-          <Route element={<PrivatRoute />}>
-            <Route path="/contacts" element={<Contacts />}></Route>
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
